02031112dd31f8e878a7523e377bde8ef714fc23
David Chaves <davi.ch.fe@gmail.com>
2020-12-19 12:12:41 -0300

    Upload do projeto

---
+++ src/test/java/com/chaves/cursospring/CursoSpringApplicationTests.java
@@ -0,0 +1,14 @@
+package com.chaves.cursospring;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {}
+
+}
